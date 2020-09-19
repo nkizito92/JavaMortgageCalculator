687f19a245ee31f336fe281e7736b855635ad4eb
Kizito Njoku <nkizito57@gmail.com>
2020-09-19 11:09:15 -0500

    added some conditions using while loops and if statements

--- src/Mortgage.java
+++ src/Mortgage.java
@@ -3,22 +3,33 @@ import java.util.Scanner;
 
 public class Mortgage {
 
-
     public static void main (String[] arg) {
         final byte percent = 100;
         final byte months_In_A_Year = 12;
-        System.out.print("Principal: ");
+        byte years = 0;
+        int principal = 0;
+        float interest = 0;
+
         Scanner scanner = new Scanner(System.in);
-        int principal = scanner.nextInt();
+        while(true) {
+              System.out.print("Principal: $");
+              principal = scanner.nextInt();
+            if(principal > 1000) break;
+        }
+           while(true)  {
+               System.out.print("Annual Interest Rate: ");
+               interest = scanner.nextFloat();
+               if(interest > 0 && interest < 30) break;
+           }
+            float monthlyInterest = interest / percent / months_In_A_Year;
 
-        System.out.print("Annual Interest Rate: ");
-        float interest = scanner.nextFloat();
-        float monthlyInterest = interest / percent / months_In_A_Year;
-
-        System.out.print("Years: ");
-        byte years = scanner.nextByte();
+        while(true) {
+            System.out.print("Years: ");
+            years = scanner.nextByte();
+            if(years > 0) break;
+            System.out.println("Most be at least a year");
+        }
         int numberOfPayments = years * months_In_A_Year;
-
         double mortgage = principal * (monthlyInterest *Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) * 1);
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
